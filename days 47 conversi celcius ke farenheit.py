fd8269ec0c98f305d2003e504627d844e946da55
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-28 17:52:13 +0800

    Add files via upload

---
+++ days 47 conversi celcius ke farenheit.py
@@ -0,0 +1,7 @@
+#mengkonversi suhu dalam derajat ke farenheit celcius
+
+nilai= float(input ("input suhu dalam derajat celcius : "))
+
+farenheit = 32+ (nilai *1.8) 
+
+print ("derajat ",nilai,"celcius sama dengan",farenheit)
